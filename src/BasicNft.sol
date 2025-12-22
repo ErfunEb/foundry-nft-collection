51e6f04f1c74688891a89ead9359374863583800
Erfan Ebrahimi <prgerfanebrahimi@gmail.com>
2025-12-22 17:13:33 +0330

    Adding BasicNft

---
+++ src/BasicNft.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+
+contract BasicNft is ERC721Upgradeable {
+    uint256 private tokenCounter;
+    mapping(uint256 => string) private tokenIdToUri;
+
+    constructor() ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function mintNft(string memory tokenUri) public {
+        tokenIdToUri[tokenCounter] = tokenUri;
+        _safeMint(msg.sender, tokenCounter);
+        tokenCounter++;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return tokenIdToUri[tokenId];
+    }
+}
